a0ef3b9c04b149fa0bcf5de87b6982f886922195
Ray <code@liquibits.com>
2024-02-26 12:53:12 -0800

    FHS Without Switcher

--- flake.nix
+++ flake.nix
@@ -10,38 +10,20 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        swts = pkgs.writeShellApplication {
-          name = "nvim-switch";
-          runtimeInputs = with pkgs; [
-            neovim
-            neovim-qt
+      in
+      {
+        packages.default = pkgs.buildFHSEnv {
+          name = "nvim";
+          targetPkgs = pkgs: (with pkgs; [
             stdenv.cc
             gnumake
             rustc
             cargo
-          ];
-          text = builtins.readFile ./nvim-switch.sh;
-        };
-        swtc = pkgs.buildFHSEnv {
-          name = "nvim-switch";
-          runScript = "${swts}/bin/nvim-switch";
+          ]);
+          runScript = "${pkgs.neovim}/bin/nvim";
           extraBwrapArgs = [
-            "--ro-bind /init /init"
             "--bind /etc/nixos /etc/nixos"
           ];
-        };
-      in
-      {
-        packages.default = pkgs.stdenvNoCC.mkDerivation {
-          pname = "neovim-lazyvim";
-          version = "0.1";
-          dontUnpack = true;
-          installPhase = ''
-            mkdir -p $out/bin
-            ln -s "${swtc}/bin/nvim-switch" $out/bin/nvim
-            ln -s "${swtc}/bin/nvim-switch" $out/bin/nvim-qt
-            ln -s "${pkgs.neovim-qt}/share" $out/share
-          '';
         };
       });
 }
